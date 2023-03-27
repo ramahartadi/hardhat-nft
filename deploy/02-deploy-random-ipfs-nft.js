17cdeff50446d3b9eb1c71fd279d5b0d11a9433f
ramahartadi <ramahartadi@gmail.com>
2023-03-27 16:20:06 +0700

    first commit

---
+++ deploy/02-deploy-random-ipfs-nft.js
@@ -0,0 +1,95 @@
+const { network } = require("hardhat")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+const { storeImages, storeTokenUriMetadata } = require("../utils/uploadToPinata")
+const imagesLocation = "./images/randomNft/"
+const FUND_AMOUNT = "1000000000000000000000"
+const metadataTemplate = {
+    name: "",
+    description: "",
+    image: "",
+    attributes: [
+        {
+            trait_type: "Cuteness",
+            value: 100,
+        },
+    ],
+}
+let tokenUris = [
+    "ipfs://QmaVkBn2tKmjbhphU7eyztbvSQU5EXDdqRyXZtRhSGgJGo",
+    "ipfs://QmYQC5aGZu2PTH8XzbJrbDnvhj3gVs7ya33H9mqUNvST3d",
+    "ipfs://QmZYmH5iDbD6v3U2ixoVAjioSzvWJszDzYdbeCLquGSpVm",
+]
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    let vrfCoordinatorV2Address, subscriptionId, vrfCoordinatorV2Mock
+
+    const chainId = network.config.chainId
+
+    if (process.env.UPLOAD_TO_PINATA == "true") {
+        tokenUris = await handleTokenUris()
+    }
+
+    if (chainId == 31337) {
+        // create VRFV2 Subscription
+        vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+        const transactionResponse = await vrfCoordinatorV2Mock.createSubscription()
+        const transactionReceipt = await transactionResponse.wait()
+        subscriptionId = transactionReceipt.events[0].args.subId
+        // Fund the subscription
+        // Our mock makes it so we don't actually have to worry about sending fund
+        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, FUND_AMOUNT)
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2
+        subscriptionId = networkConfig[chainId].subscriptionId
+    }
+
+    log("----------------------------------------------------")
+
+    arguments = [
+        vrfCoordinatorV2Address,
+        subscriptionId,
+        networkConfig[chainId]["gasLane"],
+        networkConfig[chainId]["mintFee"],
+        networkConfig[chainId]["callbackGasLimit"],
+        tokenUris,
+    ]
+    const randomIpfsNft = await deploy("RandomIpfsNft", {
+        from: deployer,
+        args: arguments,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    if (chainId == 31337) {
+        await vrfCoordinatorV2Mock.addConsumer(subscriptionId, randomIpfsNft.address)
+    }
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(randomIpfsNft.address, arguments)
+    }
+}
+
+async function handleTokenUris() {
+    // Check out https://github.com/PatrickAlphaC/nft-mix for a pythonic version of uploading
+    // to the raw IPFS-daemon from https://docs.ipfs.io/how-to/command-line-quick-start/
+    // You could also look at pinata https://www.pinata.cloud/
+    tokenUris = []
+    const { responses: imageUploadResponses, files } = await storeImages(imagesLocation)
+    for (imageUploadResponseIndex in imageUploadResponses) {
+        let tokenUriMetadata = { ...metadataTemplate }
+        tokenUriMetadata.name = files[imageUploadResponseIndex].replace(".png", "")
+        tokenUriMetadata.description = `An adorable ${tokenUriMetadata.name} pup!`
+        tokenUriMetadata.image = `ipfs://${imageUploadResponses[imageUploadResponseIndex].IpfsHash}`
+        console.log(`Uploading ${tokenUriMetadata.name}...`)
+        const metadataUploadResponse = await storeTokenUriMetadata(tokenUriMetadata)
+        tokenUris.push(`ipfs://${metadataUploadResponse.IpfsHash}`)
+    }
+    console.log("Token URIs uploaded! They are:")
+    console.log(tokenUris)
+    return tokenUris
+}
+
+module.exports.tags = ["all", "randomipfs", "main"]
---
+++ test/unit/basicNft.test.js
@@ -0,0 +1,16 @@
+const { assert } = require("chai")
+const { network, deployments, ethers } = require("hardhat")
+const { developmentChains } = require("../../helper-hardhat-config")
+
+!developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("basicNft Unit Tests", function () {
+          let basicNft, deployer
+
+          beforeEach(async () => {
+              accounts = await ethers.getSigners()
+              deployer = accounts[0]
+              await deployments.fixture(["basicnft"])
+              basicNft = await ethers.getContract("BasicNft")
+          })
+      })
